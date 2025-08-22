e0adf3616147342694f6f16c1f48e4448adfe2c7
anthonir30mp-lab <anthonir30mp@gmail.com>
2025-08-22 16:32:54 -0500

    Update pilitas6.py

--- pilitas6.py
+++ pilitas6.py
@@ -1,12 +1,36 @@
-pila=[]
+class Stack:
+  def __init__(self):
+    self.stack = []
 
-pila.append('A')
-pila.append('B')
-pila.append('C')
+  def push(self, element):
+    self.stack.append(element)
 
-print("Stack: ", pila.stack)
-print("Pop: ", pila.pop())
-print("Stack after Pop: ", pila.stack)
-print("Peek: ", pila.peek())
-print("isEmpty: ", pila.isEmpty())
-print("Size: ", pila.size())
+  def pop(self):
+    if self.isEmpty():
+      return "Stack is empty"
+    return self.stack.pop()
+
+  def peek(self):
+    if self.isEmpty():
+      return "Stack is empty"
+    return self.stack[-1]
+
+  def isEmpty(self):
+    return len(self.stack) == 0
+
+  def size(self):
+    return len(self.stack)
+
+# Create a stack
+myStack = Stack()
+
+myStack.push('A')
+myStack.push('B')
+myStack.push('C')
+
+print("Stack: ", myStack.stack)
+print("Pop: ", myStack.pop())
+print("Stack after Pop: ", myStack.stack)
+print("Peek: ", myStack.peek())
+print("isEmpty: ", myStack.isEmpty())
+print("Size: ", myStack.size())
